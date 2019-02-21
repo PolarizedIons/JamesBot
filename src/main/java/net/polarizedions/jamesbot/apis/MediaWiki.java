ef44fe3ac162e28b1b7048b8deb27fea4eb27e38
PolarizedIons <polarizedions@gmail.com>
2019-02-21 19:44:19 +0200

    minecraft wiki search

---
+++ src/main/java/net/polarizedions/jamesbot/apis/MediaWiki.java
@@ -0,0 +1,25 @@
+package net.polarizedions.jamesbot.apis;
+
+import com.google.gson.JsonObject;
+import org.jetbrains.annotations.Nullable;
+
+public class MediaWiki {
+    @Nullable
+    public static String searchPage(String domain, String query) {
+        String url = domain + "/api.php?action=query&list=search&utf8=&format=json&srsearch=" + APIUtil.encodeURIComponent(query);
+        JsonObject json = APIUtil.getJson(url);
+
+        if (json == null) {
+            return null;
+        }
+
+        json = json.getAsJsonObject("query");
+        if (json.getAsJsonObject("searchinfo").get("totalhits").getAsInt() == 0) {
+            return null;
+        }
+
+        JsonObject result = json.getAsJsonArray("search").get(0).getAsJsonObject();
+
+        return domain + "/" + result.get("title").getAsString();
+    }
+}
---
+++ src/main/java/net/polarizedions/jamesbot/modules/chat/MinecraftWiki.java
@@ -0,0 +1,53 @@
+package net.polarizedions.jamesbot.modules.chat;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.polarizedions.jamesbot.apis.MediaWiki;
+import net.polarizedions.jamesbot.commands.ICommand;
+import net.polarizedions.jamesbot.commands.brigadier.ReturnConstants;
+import net.polarizedions.jamesbot.modules.Module;
+import net.polarizedions.jamesbot.utils.CommandMessage;
+
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
+import static net.polarizedions.jamesbot.commands.brigadier.TypeFixer.argument;
+import static net.polarizedions.jamesbot.commands.brigadier.TypeFixer.literal;
+
+public class MinecraftWiki extends Module implements ICommand {
+    @Override
+    public void register(CommandDispatcher<CommandMessage> dispatcher) {
+        dispatcher.register(
+                literal("mwiki")
+                    .then(
+                            argument("query", greedyString()).executes(c -> this.search(c.getSource(), getString(c, "query")))
+                    )
+        );
+    }
+
+    private int search(CommandMessage source, String query) {
+        String url = MediaWiki.searchPage("https://minecraft.gamepedia.com", query);
+        if (url == null) {
+            source.respond("Sorry, I couldn't find that page :(");
+
+            return ReturnConstants.FAIL_REPLIED;
+        }
+
+        source.respond(url);
+        return ReturnConstants.SUCCESS;
+    }
+
+
+    @Override
+    public String getHelp() {
+        return "Finds a Minecraft Wiki page";
+    }
+
+    @Override
+    public String getUsage() {
+        return "mwiki <query>";
+    }
+
+    @Override
+    public String getModuleName() {
+        return "mwiki";
+    }
+}
